bf937670407aa4b57da5c96a1c99f04cd5843fc6
BT4DT <149670623+BT4DT@users.noreply.github.com>
2026-05-06 08:35:04 +0000

    Update merge.sh

--- merge.sh
+++ merge.sh
@@ -1,80 +1,49 @@
 #!/bin/bash
 
-rm -f merged.txt raw.txt clean.txt whitelist.txt final.txt
+rm -f raw.txt merged_clean.txt final.txt whitelist.txt
 
 # ===== list =====
 urls=(
+# =========== IRN
+# ===  🟢 Persian -PersianBlocker-Deprecated.txt :: Persian
 "https://github.com/MasterKia/PersianBlocker/raw/refs/heads/main/PersianBlocker-Deprecated.txt"
-"https://github.com/MasterKia/PersianBlocker/raw/refs/heads/main/PersianBlocker.txt"
+# ===  🟢 Persian -PersianBlockerAds-Domains.txt:: Persian
 "https://github.com/MasterKia/PersianBlocker/raw/refs/heads/main/PersianBlockerAds-Domains.txt"
+# ===  🟢 Persian -PersianBlockerAds-Hosts.txt:: Persian
 "https://github.com/MasterKia/PersianBlocker/raw/refs/heads/main/PersianBlockerAds-Hosts.txt"
+# ===  🟢 Persian -PersianBlockerAds.txt:: Persian
 "https://github.com/MasterKia/PersianBlocker/raw/refs/heads/main/PersianBlockerAds.txt"
-"https://github.com/MasterKia/PersianBlocker/raw/refs/heads/main/PersianBlockerAnnoyances-Domains.txt"
-"https://github.com/MasterKia/PersianBlocker/raw/refs/heads/main/PersianBlockerAnnoyances.txt"
-"https://github.com/MasterKia/PersianBlocker/raw/refs/heads/main/PersianBlockerCensor-Domains.txt"
-"https://github.com/MasterKia/PersianBlocker/raw/refs/heads/main/PersianBlockerCensor.txt"
-"https://github.com/MasterKia/PersianBlocker/raw/refs/heads/main/PersianBlockerHalfPrice.txt"
+# ===  🟢 Persian -PersianBlockerHosts.txt:: Persian
 "https://github.com/MasterKia/PersianBlocker/raw/refs/heads/main/PersianBlockerHosts.txt"
-"https://github.com/MasterKia/PersianBlocker/raw/refs/heads/main/PersianBlockerMalware.txt"
-"https://github.com/MasterKia/PersianBlocker/raw/refs/heads/main/PersianBlockerMobile.txt"
-"https://github.com/MasterKia/PersianBlocker/raw/refs/heads/main/PersianBlockerPhishing.txt"
-"https://github.com/MasterKia/PersianBlocker/raw/refs/heads/main/PersianBlockerSMS.txt"
-"https://github.com/MasterKia/PersianBlocker/raw/refs/heads/main/PersianBlockerTrackers-Domains.txt"
-"https://github.com/MasterKia/PersianBlocker/raw/refs/heads/main/PersianBlockerTrackers-Hosts.txt"
-"https://github.com/MasterKia/PersianBlocker/raw/refs/heads/main/PersianBlockerTrackers.txt"
-"https://github.com/MasterKia/PersianBlocker/raw/refs/heads/main/hosts"
-"https://github.com/MasterKia/PersianBlocker/raw/refs/heads/main/hosts-0"
-"https://adguardteam.github.io/HostlistsRegistry/assets/filter_19.txt"
 )
 
+# ===== download =====
 for url in "${urls[@]}"; do
   curl -sL "$url" >> raw.txt
+  echo -e "\n" >> raw.txt
 done
 
-# ===== extract domain =====
-grep -Eo '([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}' raw.txt > clean.txt
+# ===== clean basic =====
+grep -vE '^\s*$' raw.txt | \
+grep -vE 'localhost|localdomain|broadcasthost' > cleaned.txt
 
-# ===== Del junk =====
-grep -vE 'localhost|localdomain|broadcasthost' clean.txt > tmp.txt
+# ===== remove duplicate =====
+sort -u cleaned.txt > merged_clean.txt
 
-# ===== convert adblock =====
-sed 's/^/||/' tmp.txt | sed 's/$/^/' > merged.txt
-
-# ===== Del duplicate =====
-sort -u merged.txt > merged_clean.txt
-
-# ===== Make whitelist =====
+# ===== whitelist =====
 cat <<EOF > whitelist.txt
-@@||gstatic.com^
-@@||digikala.com^
-@@||snapp.ir^
-@@||soft98.ir^
-@@||snappfood.ir^
-@@||cafebazaar.ir^
-@@||divar.ir^
-@@||sheypoor.com^
-@@||aparat.com^
-@@||namava.ir^
-@@||filimo.com^
-@@||irancell.ir^
-@@||mci.ir^
-@@||soft98.ir^
-@@||shaparak.ir^
-@@||zarinpal.com^
+# ==== WHITELIST ====
+# remove # to enable
+# @@||google.com^
+# @@||gstatic.com^
+# @@||cloudflare.com^
 @@||dnsforge.de^
-@@||mymax.top^
-@@||plusiptv.dnsz.in^
-@@||plusiptv.tvdns.top^
-@@||plusiptv.dnsset.site^
-@@||alibaba.ir^
-@@||idpay.ir^
 EOF
 
-# ===== Final =====
+# ===== final =====
 cat whitelist.txt merged_clean.txt > final.txt
 
-# ===== Export =====
 mv final.txt merged.txt
 
-# ===== Clean =====
-rm raw.txt clean.txt tmp.txt merged_clean.txt whitelist.txt
+# ===== clean =====
+rm raw.txt cleaned.txt merged_clean.txt whitelist.txt
